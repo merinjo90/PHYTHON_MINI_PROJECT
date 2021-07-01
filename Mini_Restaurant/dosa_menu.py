a1ab705e830d61ece8cedd1e988a42212f4a5a21
merinjo90 <merinjo90@gmail.com>
2021-07-01 11:38:57 +0530

    add menu:list of dosa

--- Mini_Restaurant/dosa_menu.py
+++ Mini_Restaurant/dosa_menu.py
@@ -4,4 +4,21 @@ def Mini_dosaCorner():
     print("Subject: python")
     print("")
     print("TOPIC: Mini Restaurant DosaCorner")
+    print("")
+
+    print("$"*40,"WELCOME TO WHITE MINIDOSACORNER","$"*40)
+    print("")
+    print("$"*30,"SELECT YOUR FAVOURITE AND YUMMY DOSA !!!!!!!!!!!","$"*30)
+    print("")
+    print("MENU\n")
+    print("1 SADA DOSA @ RS.50")
+    print("2 CHEESE DOSA @ RS.180")
+    print("3 SECHEZWAN DOSA @ RS.150")
+    print("4 MASALA DOSA @ RS.90")
+    print("5 SWEET CORN DOSA @ RS.120")
+    print("6 CHEESE MASALA DOSA @ RS.200")
+
+    
+
+
 Mini_dosaCorner()
