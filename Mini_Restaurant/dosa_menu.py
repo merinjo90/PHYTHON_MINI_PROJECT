f4aa47bdb6f9bc5d1dc838e04d5318dccd680b24
merinjo90 <merinjo90@gmail.com>
2021-07-01 11:27:07 +0530

    initial commit

---
+++ Mini_Restaurant/dosa_menu.py
@@ -0,0 +1,7 @@
+def Mini_dosaCorner():
+    print("Name: Merin Joseph")
+    print("")
+    print("Subject: python")
+    print("")
+    print("TOPIC: Mini Restaurant DosaCorner")
+Mini_dosaCorner()
