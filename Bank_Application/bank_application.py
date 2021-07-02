b674713914ef30f525313fb97d50d4d6a42702c8
merinjo90 <merinjo90@gmail.com>
2021-07-02 14:29:23 +0530

    Question created

---
+++ Bank_Application/bank_application.py
@@ -0,0 +1,12 @@
+
+"""
+#create a bank application.
+#          : "Account"-parent class, with a "BankName: ABC bank,IFSCcode : 45154,Balance" as
+#          class variables. inital balace "10000"common to all customer.
+#         :"AccountHolder"-child class, with instance variables "name,AccNo" and functions
+#         "Deposit,widrow,Balacecheck" .
+#         : if the user use any of these method by passing an amount, we want to get full
+#         bank details and  current balance .
+#         : for balance checking dont want to pass any amount.
+#         : provided min balance required is limited to 1000.
+"""
