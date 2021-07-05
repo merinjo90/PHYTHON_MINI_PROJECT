a87be649fcde660428b1cea1d5938df143a96237
merinjo90 <merinjo90@gmail.com>
2021-07-05 08:03:42 +0530

    create a GUI window and start the GUI

---
+++ Calculate_yourAge/age_calculate.py
@@ -0,0 +1,18 @@
+#import all functn frm the tkinter
+from tkinter import*
+from tkinter import messagebox
+
+
+
+#create gui window
+ageCalculator= Tk()
+#set background clr f gui window
+ageCalculator.configure(background="light yellow")
+#set name of gui window
+ageCalculator.title("AGE CALCULATOR")
+#set congifuration of window
+ageCalculator.geometry("500x250")
+#Create a DOB label
+
+
+ageCalculator.mainloop()
