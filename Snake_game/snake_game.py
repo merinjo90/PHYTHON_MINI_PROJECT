ed313d736036c32374b3ab06c63269604cfb89c9
merinjo90 <merinjo90@gmail.com>
2021-07-06 11:25:58 +0530

    create snakehead mving up,down,left and right

--- Snake_game/snake_game.py
+++ Snake_game/snake_game.py
@@ -1,21 +1,46 @@
 import turtle
+import time
+
+delay=0.1
 
 #creating turtle screen
 screen=turtle.Screen()
 screen.title("SNAKE GAME")
 screen.bgcolor("light gray")
 screen.setup(width=600,height=600)
-#screen.tracer(0)
+screen.tracer(0)
 
-#snake
+#snake head
 snake=turtle.Turtle()
 snake.shape("square")
 snake.color("green")
 snake.speed(0)
 snake.penup()
 snake.goto(0,0)
-snake.direction="stop"
+snake.direction="right"
+
+#snake head moving
+def snakeMove():
+    if snake.direction=="up":
+        y=snake.ycor()
+        snake.sety(y+20)
+    if snake.direction=="down":
+        y=snake.ycor()
+        snake.sety(y-20)
+    if snake.direction=="left":
+        x=snake.xcor()
+        snake.setx(x-20)
+    if snake.direction=="right":
+        x=snake.xcor()
+        snake.setx(x+20)
 
 
 
+while True:
+    screen.update()
+    snakeMove()
+    time.sleep(delay)
+
+
+#turtle.Terminator()
 screen.mainloop()
