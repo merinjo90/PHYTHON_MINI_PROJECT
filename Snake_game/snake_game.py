107dd5a8e2265eb9400964165d3737755d7fa9cd
merinjo90 <merinjo90@gmail.com>
2021-07-06 10:22:07 +0530

    creating turtle screen

--- Snake_game/snake_game.py
+++ Snake_game/snake_game.py
@@ -1,3 +1,11 @@
 import turtle
 import random
 import time
+
+#creating turtle screen
+screen=turtle.Screen()
+screen.title("SNAKE GAME")
+screen.bgcolor("light blue")
+screen.setup(width=600,height=600)
+screen.tracer(0)
+turtle.Terminator()
