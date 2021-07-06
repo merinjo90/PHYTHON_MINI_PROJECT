4f30dae7abbceea4e96ef6a288067f630e5cb73e
merinjo90 <merinjo90@gmail.com>
2021-07-06 11:03:40 +0530

    creating snake

--- Snake_game/snake_game.py
+++ Snake_game/snake_game.py
@@ -5,7 +5,16 @@ screen=turtle.Screen()
 screen.title("SNAKE GAME")
 screen.bgcolor("light gray")
 screen.setup(width=600,height=600)
-screen.tracer(0)
+#screen.tracer(0)
+
+#snake
+snake=turtle.Turtle()
+snake.shape("square")
+snake.color("green")
+snake.speed(0)
+snake.penup()
+snake.goto(0,0)
+snake.direction="stop"
 
 
 
