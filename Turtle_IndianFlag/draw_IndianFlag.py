87526889a9e875da96da9ef6f478230e55da67e0
merinjo90 <merinjo90@gmail.com>
2021-07-07 12:55:47 +0530

    import turtle and screen initialize

---
+++ Turtle_IndianFlag/draw_IndianFlag.py
@@ -0,0 +1,17 @@
+import turtle
+flag=turtle.Turtle()
+screen=turtle.Screen()
+flag.speed(4)
+flag.pensize(6)
+
+def draw(x,y):
+    flag.penup()
+    flag.goto(x,y)
+    flag.pendown()
+
+
+
+
+
+
+screen.mainloop()
