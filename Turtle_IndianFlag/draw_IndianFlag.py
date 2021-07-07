74093f182fb3c37443fb758a2295a276b56d8277
merinjo90 <merinjo90@gmail.com>
2021-07-07 13:07:48 +0530

    ashoka Chakra created

--- Turtle_IndianFlag/draw_IndianFlag.py
+++ Turtle_IndianFlag/draw_IndianFlag.py
@@ -1,14 +1,25 @@
 import turtle
+
 flag=turtle.Turtle()
 screen=turtle.Screen()
+
 flag.speed(4)
 flag.pensize(6)
+flag.color("#0541B7")
 
 def draw(x,y):
     flag.penup()
     flag.goto(x,y)
     flag.pendown()
 
+#ashoka Chakra
+for i in range(24): #24 strokes in ashok chakra
+    flag.forward(80)
+    flag.backward(80)
+    flag.left(15) #360 divide by 24 then get 15
+draw(0,-80)
+flag.circle(80,360)
+#turtle.done()
 
 
 
