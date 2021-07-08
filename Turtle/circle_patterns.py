cfdc8b7e5655fc4a3f5522fa59169f5f3471a35c
merinjo90 <merinjo90@gmail.com>
2021-07-08 13:14:49 +0530

    turtle: circle pattern created

---
+++ Turtle/circle_patterns.py
@@ -0,0 +1,70 @@
+import turtle
+t=turtle.Turtle()
+window=turtle.Screen()
+t.up()
+t.goto(200,0)
+
+def circle1():
+    t.up()
+    t.goto(200, 0)
+    list=["teal","lightcoral","darkviolet","saddlebrown"]
+    for i in range(4):
+        t.down()
+        t.begin_fill()
+        t.fillcolor(list[i])
+        t.circle(100)
+        t.end_fill()
+        t.up()
+        t.bk(120)
+circle1()
+t.reset()
+
+
+def circle2():
+    t.up()
+    t.goto(200, 0)
+    list=["yellow","red","blue","green"]
+    for i in range(4):
+        t.down()
+        t.begin_fill()
+        t.fillcolor(list[i])
+        t.circle(50)
+        t.end_fill()
+        t.up()
+        t.bk(100)
+circle2()
+t.reset()
+
+
+def circle3():
+    t.up()
+    t.goto(200, 0)
+    list=["yellow","red","blue","green"]
+    for i in range(4):
+        t.down()
+        t.begin_fill()
+        t.fillcolor(list[i])
+        t.pensize(10)
+        t.circle(50)
+        t.end_fill()
+        t.up()
+        t.bk(100)
+circle3()
+t.reset()
+
+def circle4():
+    t.up()
+    t.goto(200, 0)
+    list=["yellow","red","blue","green"]
+    for i in range(4):
+        t.down()
+        t.color(list[i])
+        t.pensize(20)
+        t.circle(100)
+        t.up()
+        t.bk(100)
+circle4()
+#t.reset()
+
+
+window.mainloop()
