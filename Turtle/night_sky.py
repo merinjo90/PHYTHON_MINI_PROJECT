e8271837749a524599b825147f71c8345e180ddb
merinjo90 <merinjo90@gmail.com>
2021-07-09 12:07:50 +0530

    we have created 3 object moon,star,text

--- Turtle/night_sky.py
+++ Turtle/night_sky.py
@@ -1,8 +1,23 @@
 import turtle
 import random
 
+#window setup
 window=turtle.Screen()
+window.setup(width=800,height=600)#window size
+window.bgcolor("black")#background clr
+
+moon=turtle.Turtle()
+moon.hideturtle()
+
+
+star=turtle.Turtle()
+star.hideturtle()
+
+
+text=turtle.Turtle()
+text.hideturtle()
 
 
 
-window.mainloop()
+turtle.done()
+#window.mainloop()
