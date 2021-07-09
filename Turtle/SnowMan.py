5179dbf1a6c237cf363ac44a5524d1347139a4fe
merinjo90 <merinjo90@gmail.com>
2021-07-09 14:35:52 +0530

    create a blue background for snowman

---
+++ Turtle/SnowMan.py
@@ -0,0 +1,19 @@
+import turtle
+
+window=turtle.Screen()
+#turtle=turtle.Turtle()
+turtle.speed(0)
+turtle.setup(width=800,height=700)
+
+#blue background
+turtle.penup()
+turtle.goto(0,-320)
+turtle.pendown()
+turtle.color("lightskyblue")
+turtle.begin_fill()
+turtle.circle(300)
+turtle.end_fill()
+
+
+turtle.done
+window.mainloop()
